6336c292e8f6d82788beb03b18716b3f2720fff8
rollen <rafal.wartalski@gmail.com>
2014-10-03 00:23:42 +0200

    Configuration moved to Config Bundle

---
+++ src/Rav/ConfigBundle/DependencyInjection/RavConfigExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rav\ConfigBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class RavConfigExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('partner.default_val', 'Done from config Bundle');
+
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ src/Rav/ConfigBundle/RavConfigBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rav\ConfigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RavConfigBundle extends Bundle
+{
+}
